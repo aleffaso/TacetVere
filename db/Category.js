50d34c53a88e17c3533e5f0e63cae38806445413
Aleff <aleff.a.s.o@gmail.com>
2022-05-22 19:20:43 -0300

    structuring blog page

---
+++ db/Category.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const connection = require("./db");
+
+const Category = connection.define('categories', {
+    title:
+    {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: 
+    {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+Category.sync({force:false}).then(() => {}); //Create table in case of it dos not exist
+
+module.exports = Category;
