01267df0adf4f724f2d2dcb4b36c464e55575753
Aleff <aleff.a.s.o@gmail.com>
2022-12-09 11:03:30 -0300

    hash

--- config/calculators.js
+++ config/calculators.js
@@ -3,9 +3,13 @@ const app = express.Router();
 
 // const CarbonManagement = require('../db/CarbonManagement');
 
-app.get('/greener', (req, res) => {
+app.get('/2b2062936e9bb5a6b1d17869cc48c3934142b563', (req, res) => {
     res.render('pages/calculator/carbonManagement/index'); 
 })
+
+// app.get('/greener', (req, res) => {
+//     res.render('pages/calculator/carbonManagement/index'); 
+// })
 
 // app.post('/greener-carbon-managament', (req, res) => {
 //     console.log(req.body)
