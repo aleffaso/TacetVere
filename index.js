510b9a282114af63c8005e23b4e2e0f95b6b9c9e
Aleff <aleff.a.s.o@gmail.com>
2023-05-11 15:32:37 -0300

    Removing calculator

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const session = require('express-session');
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
-const connection = require("./db/db")
+const connection = require("./db/db");
 const categoriesController = require("./controllers/categories");
 const articlesController = require("./controllers/articles");
 const usersController = require("./controllers/users");
 const newsletter = require("./controllers/newsletter");
 const calculator = require("./controllers/calculator");
-const routes = require('./config/routes');
-const calcs = require('./config/calculators');
-const dotenv = require('dotenv');
+const routes = require("./config/routes");
+const calcs = require("./config/calculators");
+const dotenv = require("dotenv");
 
 //Set envkeys
-dotenv.config({path: './.env'})
+dotenv.config({ path: "./.env" });
 
 //View engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Sessions
-app.use(session({
-    secret: process.env.SESSION_SECRET, 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
 //Static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //database connection
 connection.authenticate().then(() => {
-    try {
-        console.log("connection success");
-    } catch (error) {
-        console.log(error);
-    }
-})
+  try {
+    console.log("connection success");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //routes
 app.use("/", articlesController);
@@ -48,10 +50,10 @@ app.use("/", categoriesController);
 app.use("/", usersController);
 app.use("/", newsletter);
 app.use("/", calculator);
-app.use("/", calcs);
+// app.use("/", calcs);
 app.use("/", routes);
 
 //Server
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running");
-})
+  console.log("Server is running");
+});
